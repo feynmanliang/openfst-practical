79d23d88ad95994e53306754d6c4b62d29bf5832
Feynman Liang <feynman.liang@gmail.com>
2016-03-11 10:33:30 +0000

    Finished preliminary

--- 2-prelim-q.zsh
+++ 2-prelim-q.zsh
@@ -2,8 +2,8 @@
 
 # Draw the input and model FSTs generated above. Then compose them and project
 # the result to keep only output symbols, and draw the resulting FST
-# (result.fst). Note that when drawing acceptors (such as ’input’), you can
-# optionally use ’fstdraw --acceptor’ to avoid showing the redundant output
+# (result.fst). Note that when drawing acceptors (such as 'input'), you can
+# optionally use `fstdraw --acceptor` to avoid showing the redundant output
 # symbols (only showing input ones).
 fstdraw --isymbols=isyms.txt --osymbols=isyms.txt --acceptor input.fst | dot -Tps > input.ps
 fstdraw --isymbols=isyms.txt --osymbols=osyms.txt model.fst | dot -Tps > model.ps
@@ -11,18 +11,29 @@ fstdraw --isymbols=isyms.txt --osymbols=osyms.txt model.fst | dot -Tps > model.p
 fstarcsort --sort_type=ilabel model.fst \
   | fstcompose input.fst - \
   | fstproject --project_output - result.fst
-fstdraw --isymbols=isyms.txt --osymbols=osyms.txt result.fst | dot -Tps > result.ps
+fstdraw --isymbols=osyms.txt --osymbols=osyms.txt --acceptor result.fst | dot -Tps > result.ps
 
 # (i) How many alternative paths exist in the input FST? How many distinct
 # strings do these encode?  Which ones of these strings can be accepted by the
 # model transducer?
+# A: 4 paths, encoding 4 strings, 3 of which get accepted by the model ('a b c
+# a' is not)
+printstrings.O2 --label-map=isyms.txt --input=input.fst -n 10 -w 2> /dev/null
 
 # (ii) How many alternative paths exist in the result FST? How many distinct
 # strings do these encode?
+# A: 4 paths, encoding 3 distinct strings
+printstrings.O2 --label-map=osyms.txt --input=result.fst -n 10 -w 2> /dev/null
 
 # (iii) Now remove epsilon arcs, determinize and minimize the result FST. How
 # many alternative paths exist now? How many distinct strings do these encode?
 # Why?
+# A: 3 paths, encoding 3 distinct strings. This is because determinization
+# leaves one distinct path per string.
+fstrmepsilon result.fst | fstdeterminize | fstminimize - result2.fst
+printstrings.O2 --label-map=osyms.txt --input=result2.fst -n 10 -w 2> /dev/null
 
 # (iv) How many alternative strings are accepted by the model FST? Refer to the
 # drawing to support your answer
+# A: An infinite number of strings, because the loop at state 3 accepts any
+# unlimited sequence of 'a' labels.
